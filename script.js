706a82b46893370c10b295554caea1f26a99c462
Md.Mohiul Islam Miraz <mohiulislam23@gmail.com>
2024-08-27 23:17:46 +0600

    FAQ and Footer

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
+
+/*----------------------------------------- header -----------------------------------------*/
+
 window.addEventListener('scroll', function(){
     const header = document.querySelector('header');
     
     if (window.scrollY > 0) header.classList.add('scrolled');
     else header.classList.remove('scrolled');
 });
+
+/*----------------------------------------- faq -----------------------------------------*/
+
+function faq(element){
+    const content = element.nextElementSibling;
+    const icon = element.querySelector('.faq-icon');
+
+    content.classList.toggle('active');
+    icon.classList.toggle('rotated');
+}
